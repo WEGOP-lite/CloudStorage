fd1a2b3535af227c897d4fac0e8c5b604edffd25
WEGOP-lite <2298711707@qq.com>
2023-07-04 03:19:34 +0800

    具体业务

---
+++ cloudstorage-item/cloudstorage-item-service/src/main/java/com/cloudstorage/CloudStorageItemApplication.java
@@ -0,0 +1,19 @@
+package com.cloudstorage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wegop
+ * @date 2023/7/4 3:00
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.cloudstorage.item.mapper")
+public class CloudStorageItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageItemApplication.class,args);
+    }
+}
