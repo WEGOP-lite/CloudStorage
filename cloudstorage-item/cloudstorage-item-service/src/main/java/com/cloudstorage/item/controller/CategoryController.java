5a7776222d7f721090ea64b2350ac13cbc5a24c4
WEGOP-lite <2298711707@qq.com>
2023-07-05 04:29:15 +0800

    建立controller

---
+++ cloudstorage-item/cloudstorage-item-service/src/main/java/com/cloudstorage/item/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.cloudstorage.item.controller;
+
+/**
+ * @author wegop
+ * @date 2023/7/5 4:26
+ */
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据parentId查询类目
+     * @param pid
+     * @return
+     */
+    @RequestMapping("list")
+    public ResponseEntity<List<Category>> queryCategoryListByParentId(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        try {
+            if (pid == null || pid.longValue() < 0){
+                // pid为null或者小于等于0，响应400
+                return ResponseEntity.badRequest().build();
+            }
+            // 执行查询操作
+            List<Category> categoryList = this.categoryService.queryCategoryListByParentId(pid);
+            if (CollectionUtils.isEmpty(categoryList)){
+                // 返回结果集为空，响应404
+                return ResponseEntity.notFound().build();
+            }
+            // 响应200
+            return ResponseEntity.ok(categoryList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 响应500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+}
